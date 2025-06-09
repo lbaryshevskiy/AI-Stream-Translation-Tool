11365b65b4b9225de292251f98e0cbb2c1398cf9
Oleksii Bsryshevskyi <90308120+lbaryshevskiy@users.noreply.github.com>
2025-06-09 15:17:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ translator = Translator()
 
 # --- Flask App Setup ---
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode="threading")
+socketio = SocketIO(app, cors_allowed_origins="*")
 audio_queue = queue.Queue()
 
 # --- Audio Recording Thread ---
