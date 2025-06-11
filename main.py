1c46fc104065c1cf85a0fd2158cd39fe7e839144
Oleksii Bsryshevskyi <90308120+lbaryshevskiy@users.noreply.github.com>
2025-06-11 00:38:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ def launch_overlay():
 
 root = tk.Tk()
 root.title("Streamsub")
-root.geometry("380x150")
+root.geometry("380x250")
 root.resizable(True, True)
 
 frame = tk.Frame(root, padx=20, pady=20)
