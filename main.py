32c802d730bd0d3461be44f2563bd31064b84617
Oleksii Bsryshevskyi <90308120+lbaryshevskiy@users.noreply.github.com>
2025-06-11 12:54:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ header.grid(row=0, column=0, columnspan=2, pady=(0, 10))
 tk.Label(frame, text="Translate subtitles to:", font=("Helvetica", 10)).grid(row=1, column=0, sticky="e")
 selected_lang = tk.StringVar(value="Select Language")
 lang_menu = tk.OptionMenu(frame, selected_lang, *language_options.keys())
-lang_menu.config(width=15)
+lang_menu.config(width=17)
 lang_menu.grid(row=0, column=0, pady=(20, 15), padx=20, sticky="w")
 
 # OBS URL
