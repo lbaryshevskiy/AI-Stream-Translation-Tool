6ac8e2096f2bde26d6d32a0447b0c638a2da26cd
Oleksii Bsryshevskyi <90308120+lbaryshevskiy@users.noreply.github.com>
2025-06-13 21:10:26 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,16 @@
 from setuptools import setup
 
 APP = ['main.py']
-DATA_FILES = []
 OPTIONS = {
-    'iconfile': 'streamsub.icns',
-    'packages': ['encodings', 'tkinter', 'customtkinter', 'flask', 'flask_socketio', 'whisper'],
-    'excludes': [
-        'PyQt5', 'PyQt6', 'matplotlib', 'email', 'unittest',
-        'PySide2', 'PySide6', 'gi', 'gi.repository', 'cv2',
-        'tests', 'PyInstaller', 'pyqtgraph', 'torchvision',
-        'numpy.core.tests', 'numpy.random._examples'
-    ],
-    'includes': ['idna', 'requests'],
     'argv_emulation': True,
+    'iconfile': 'streamsub.icns',
+    'packages': ['customtkinter', 'flask', 'flask_socketio', 'whisper'],
+    'excludes': ['tkinter.test', 'unittest', 'PyQt5', 'matplotlib'],
 }
 
 setup(
     app=APP,
     name='Streamsub',
-    data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
 )
-
