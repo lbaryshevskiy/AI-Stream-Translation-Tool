d01a1532b7cd277cfb60e6c56ee6b0fcbd2a9ae4
Oleksii Bsryshevskyi <90308120+lbaryshevskiy@users.noreply.github.com>
2025-06-14 17:18:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -281,7 +281,7 @@ def main():
         font=("Helvetica", 10, "italic"),
         fg_color="transparent"
     )
-    plan_label.place(relx=1.0, rely=1.0, anchor="se", x=-10, y=-20)
+    plan_label.place(relx=1.0, rely=1.0, anchor="se", x=-10, y=-30)
 
     ctk.CTkLabel(frame, text="🎙️ Streamsub", font=("Helvetica", 16, "bold")).pack(pady=(0, 10))
 
