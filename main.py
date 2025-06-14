981add8f764d39fb4b46dc0af5c2f7c6868bb987
Oleksii Bsryshevskyi <90308120+lbaryshevskiy@users.noreply.github.com>
2025-06-14 17:06:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -281,6 +281,8 @@ def main():
         text_color=plan_colors.get(user_plan, "gray"),
         font=("Helvetica", 10, "italic")
     )
+    plan_label.place(relx=1.0, rely=0.0, anchor="ne", x=-10, y=10)
+
     
     ctk.CTkLabel(frame, text="🎙️ Streamsub", font=("Helvetica", 16, "bold")).pack(pady=(0, 10))
 
