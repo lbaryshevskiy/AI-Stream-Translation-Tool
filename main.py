8e048624e79a71b17058c50d6966d3e79dad851f
Oleksii Bsryshevskyi <90308120+lbaryshevskiy@users.noreply.github.com>
2025-06-16 13:22:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -218,7 +218,7 @@ def show_pro_preferences():
             command=update_opacity
         )    
         overlay_opacity_slider.set(1.0)
-        overlay_opacity_slider.pack()
+        overlay_opacity_slider.pack(pady=(0, 5))
 
         def update_preview(*args):
             # Update font size
