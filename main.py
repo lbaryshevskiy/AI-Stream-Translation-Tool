091390bb8ddfd0abe7df7b67cec89db8e63918b0
Oleksii Bsryshevskyi <90308120+lbaryshevskiy@users.noreply.github.com>
2025-06-16 22:08:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -230,17 +230,16 @@ def show_pro_preferences():
         preview_label.pack(padx=10, pady=8)
 
         # --- Preview Box ---
-        def update_preview(*args):
+       def update_preview(*args):
             preview_label.configure(font=("Helvetica", int(font_slider.get())))
-    
             opacity = overlay_opacity_slider.get()
-    
-             if opacity <= 0.11:
-                preview_frame.configure(fg_color="transparent")
+            if opacity <= 0.11:
+               preview_frame.configure(fg_color="transparent")
             else:
-                shade = int(26 + (opacity * 230))
-                hex_color = f"#{shade:02x}{shade:02x}{shade:02x}"
-                preview_frame.configure(fg_color=hex_color)
+               shade = int(26 + (opacity * 230))
+               hex_color = f"#{shade:02x}{shade:02x}{shade:02x}"
+               preview_frame.configure(fg_color=hex_color)
+  
 
 
 
