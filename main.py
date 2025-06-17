b0fb8253b5ff53e5f248bcffe832e18cd8798c0e
Oleksii Bsryshevskyi <90308120+lbaryshevskiy@users.noreply.github.com>
2025-06-16 23:35:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ def copy_url():
 def show_pro_preferences():
     popup = ctk.CTkToplevel()
     popup.title("Settings")
-    popup.geometry("430x430")
+    popup.geometry("430x480")
 
     from __main__ import dev_mode, dev_override_plan
     user_plan = dev_override_plan if dev_mode else "free"
