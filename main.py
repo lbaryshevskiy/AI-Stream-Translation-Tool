e305825b79be37cfc8162ea981aa52a1316e3639
Oleksii Bsryshevskyi <90308120+lbaryshevskiy@users.noreply.github.com>
2025-06-21 00:00:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -732,7 +732,7 @@ def main():
         fg_color="transparent",
         hover_color="gray20",
         text_color="white",
-        font=("Helvetica", 20)  # ⬅️ Increase from 16 to 18
+        font=("Helvetica", 25) 
     )
     reload_btn.pack(side="left")
     
