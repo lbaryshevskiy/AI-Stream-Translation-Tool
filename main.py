c447306204bd51b4ce50a33a0bffb8707fa6097c
Oleksii Bsryshevskyi <90308120+lbaryshevskiy@users.noreply.github.com>
2025-06-21 23:26:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -478,7 +478,7 @@ def show_pro_preferences():
         font_family_menu.pack(pady=(0, 10))
 
         # --- Preview Box (moved from page1 to page2) ---
-        preview_frame = ctk.CTkFrame(page2, fg_color="transparent", corner_radius=10, width=360, height=60)
+        preview_frame = ctk.CTkFrame(page2, fg_color="transparent", corner_radius=10, width=500, height=60)
         preview_frame.pack(pady=(10, 10), padx=40)
         
         preview_label = ctk.CTkLabel(
