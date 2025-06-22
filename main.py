2b590f423022423551978a73883f7aba407c0771
Oleksii Bsryshevskyi <90308120+lbaryshevskiy@users.noreply.github.com>
2025-06-21 20:55:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -306,16 +306,19 @@ def show_pro_preferences():
         
         font_family_var = ctk.StringVar(value="Inter")
         
+        preview_frame = ctk.CTkFrame(page1, fg_color="transparent", corner_radius=10, width=260, height=60)
+        preview_frame.pack(pady=(30, 10), padx=40)
+        
         preview_label = ctk.CTkLabel(
-            page1,
+            preview_frame,
             text="This is how your subtitle looks.",
             font=(font_family_var.get(), int(font_slider.get())),
-            height=50,           
-            wraplength=220,       
-            anchor="center",     
-            justify="center"    
+            wraplength=220,
+            anchor="center",
+            justify="center"
         )
-        preview_label.pack(pady=(20, 10))
+        preview_label.pack(expand=True, fill="both")
+
         
         # --- Live Preview Logic ---
         def update_preview(*args):
