5b88cb832d233066a65d9f7cb302b8f901ea5962
Oleksii Bsryshevskyi <90308120+lbaryshevskiy@users.noreply.github.com>
2025-06-26 20:57:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -479,7 +479,7 @@ def show_pro_preferences():
 
         # --- Preview Box (moved from page1 to page2) ---
         preview_frame = ctk.CTkFrame(page2, fg_color="transparent", corner_radius=10, width=500, height=60)
-        preview_frame.pack(pady=(70, 30), padx=40)
+        preview_frame.pack(pady=(90, 30), padx=40)
         update_preview()
         
         preview_label = ctk.CTkLabel(
