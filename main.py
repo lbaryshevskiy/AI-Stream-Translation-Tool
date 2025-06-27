c85f427b91ce3f4490934db35f4f5671e36283b9
Oleksii Bsryshevskyi <90308120+lbaryshevskiy@users.noreply.github.com>
2025-06-27 16:05:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -662,7 +662,7 @@ def show_pro_preferences():
                 "wraplength": wraplength,
                 "box_background": box_bg_var.get(),
             })
-                
+            #socketio.emit("subtitle", {"text": preview_label.cget("text")})
             global is_in_settings
             is_in_settings = False
             popup.destroy()
