2a66db391a8916b0357e6325282ad4da4ddbe367
Oleksii Bsryshevskyi <90308120+lbaryshevskiy@users.noreply.github.com>
2025-06-27 16:10:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -518,7 +518,7 @@ def show_pro_preferences():
             anchor="center",
             justify="center"
         )
-        preview_label.pack(expand=True, fill="both")
+        preview_label.pack(expand=True, fill="both", pady=(10, 30))
     
         if "Custom" in saved_box:
             last_custom = settings.get("last_custom_box_size", f"{saved_wrap}x100")
