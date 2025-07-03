2241db4304113ec61b24ca6ed5ad4ba287b89406
Oleksii Bsryshevskyi <90308120+lbaryshevskiy@users.noreply.github.com>
2025-07-03 16:59:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -69,8 +69,8 @@ backend_threads = []
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
-RATE = 16000
-RECORD_SECONDS = 5
+RATE = 44100
+RECORD_SECONDS = 10
 WAVE_OUTPUT_FILENAME = "temp.wav"
 pa = pyaudio.PyAudio()
 
