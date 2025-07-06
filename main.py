fd9a455237f218756eff5491d589049230850fd2
Oleksii Bsryshevskyi <90308120+lbaryshevskiy@users.noreply.github.com>
2025-07-05 20:39:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -913,7 +913,7 @@ def main():
         font=("Helvetica", 16),
         corner_radius=6
     )
-    mic_btn.pack(side="left", padx=(0, 6), pady=(10, 0))
+    mic_btn.pack(side="left", padx=(0, 6), pady=(8, 0))
 
 
     start_btn = ctk.CTkButton(
