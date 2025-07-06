c3eba8a2b7b294163a289f1385a1b4473e8178ca
Oleksii Bsryshevskyi <90308120+lbaryshevskiy@users.noreply.github.com>
2025-07-05 22:50:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -903,7 +903,7 @@ def main():
             mic_window.destroy()
             
         save_btn = ctk.CTkButton(mic_window, text="Save", command=save_mic_choice)
-        save_btn.pack(pady=40)
+        save_btn.pack(pady=30)
     
     mic_btn = ctk.CTkButton(
         btn_row,
