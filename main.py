fba197ae3f10b95511fbb602b0dee26b5b404712
Oleksii Bsryshevskyi <90308120+lbaryshevskiy@users.noreply.github.com>
2025-07-15 16:23:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ vad.set_mode(2)  # Aggressiveness 0-3 (2 is moderate suppression)
 
 # --- DEVELOPMENT MODE ---
 dev_mode = True
-dev_override_plan = "free"
+dev_override_plan = "studio"
 
 user_plan = dev_override_plan if dev_mode else "free"
 
