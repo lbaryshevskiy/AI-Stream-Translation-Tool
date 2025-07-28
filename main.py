2ab2c866e89c8d54a4f874b55cbf4a0251aa1e18
Oleksii Bsryshevskyi <90308120+lbaryshevskiy@users.noreply.github.com>
2025-07-28 13:45:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1109,13 +1109,13 @@ def main():
     bg_image_raw = Image.open("background_gradient.png")
     bg_image = CTkImage(light_image=bg_image_raw, dark_image=bg_image_raw, size=(300, 350))
 
+    bg_label = ctk.CTkLabel(master=frame, image=bg_image, text="")
+    bg_label.place(relx=0, rely=0, relwidth=1, relheight=1)
+    bg_label.lower()  # Ensure it's behind everything
+
     frame = ctk.CTkFrame(master=root, fg_color="transparent")
     frame.pack(fill="both", expand=True) 
     
-    bg_label = ctk.CTkLabel(master=frame, image=bg_image, text="")
-    bg_label.place(relx=0, rely=0, relwidth=1, relheight=1)
-
-     
 
     # Determine plan (simulate during dev)
     if dev_mode:
