0292cde0ff517f5818def468a97d6e84acdc4be6
Oleksii Bsryshevskyi <90308120+lbaryshevskiy@users.noreply.github.com>
2025-07-28 13:58:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1105,6 +1105,12 @@ def main():
     frame = ctk.CTkFrame(master=root, fg_color="transparent")
     frame.place(relx=0, rely=0, relwidth=1, relheight=1)
 
+    if bg_image:
+        bg_label = ctk.CTkLabel(master=frame, image=bg_image, text="", fg_color="transparent")
+        bg_label.place(relx=0, rely=0, relwidth=1, relheight=1)
+        bg_label.lower()
+
+
     from PIL import Image, ImageTk
     from customtkinter import CTkImage
     
