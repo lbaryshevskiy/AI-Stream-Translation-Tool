91a61b1f1cfb52ca5decb8c03e6ccfcc05826ea4
Oleksii Bsryshevskyi <90308120+lbaryshevskiy@users.noreply.github.com>
2025-07-27 20:11:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1111,11 +1111,6 @@ def main():
 
     frame = ctk.CTkFrame(master=root, fg_color="transparent")
     frame.pack(fill="both", expand=True) 
-    
-    bg_label = ctk.CTkLabel(master=frame, image=bg_image, text="")
-    bg_label.place(relx=0, rely=0, relwidth=1, relheight=1)
-
-     
 
     # Determine plan (simulate during dev)
     if dev_mode:
@@ -1273,6 +1268,10 @@ def main():
 
     listener = keyboard.Listener(on_press=on_press, on_release=on_release)
     listener.start()
+
+    bg_label = ctk.CTkLabel(master=frame, image=bg_image, text="")
+    bg_label.place(relx=0, rely=0, relwidth=1, relheight=1)
+    bg_label.lower()
 
     if user_plan == "free":
         global char_count_label
