7955ea62728d548913730c6a9215cc59277c2fc5
Oleksii Bsryshevskyi <90308120+lbaryshevskiy@users.noreply.github.com>
2025-07-30 23:56:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1003,7 +1003,7 @@ def show_pro_preferences():
             hotkey_btn.configure(state="disabled")
 
         footer_frame = ctk.CTkFrame(popup, fg_color="transparent")
-        footer_frame.pack(pady=(5, 10), fill="x", padx=(50, 0))  # shift row inward from left
+        footer_frame.pack(pady=(5, 10), fill="x")  # shift row inward from left
 
         right_frame = ctk.CTkFrame(footer_frame, fg_color="transparent")
         right_frame.pack(side="right", padx=(100, 25))
